f988abb5bb0ab6393958f56457fe48ad990a0d0f
Liam Bailey <liam.bailey@skillcorner.com>
2024-11-07 11:24:11 +0000

    Updating setup information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skillcornerviz',
-    version='1.0.8',
+    version='1.0.9',
 
     url='https://github.com/liamMichaelBailey/skillcornerviz',
     author='Liam Michael Bailey',
